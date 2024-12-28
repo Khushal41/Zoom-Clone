f66f52139fc274c850d15ffcb63501f38f346b4a
khushal41 <114830517+Khushal41@users.noreply.github.com>
2024-12-28 16:42:54 +0530

    Commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apna-video-call-hjsx.onrender.com" :
+    "https://zoom-clone-backend-kflw.onrender.com" :
 
     "http://localhost:8000"
 
