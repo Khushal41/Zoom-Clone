86fe8ea356ccb93a39e8be6621c1dd4422397338
khushal41 <114830517+Khushal41@users.noreply.github.com>
2025-02-16 17:50:49 +0530

    Commit

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import "../App.css";
 import { Link, useNavigate } from 'react-router-dom';
 
-
-
-
-
 export default function LandingPage() {
   const router = useNavigate();
 
